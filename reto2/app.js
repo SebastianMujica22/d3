3d966029f6eb325d3a9deec911d6db6b4a21684f
SebastianMujica22 <42355577+SebastianMujica22@users.noreply.github.com>
2020-05-14 00:07:10 -0500

    terminado

--- reto2/app.js
+++ reto2/app.js
@@ -1,10 +1,11 @@
 const data = [];
 
 const render = (data) => {
-
-  var margin = {top: 10, right: 20, bottom: 30, left: 50}
+  var margin = { top: 100, right: 200, bottom: 30, left: 50 };
   const width = 700;
   const height = 400;
+  const inWidth = width - margin.left - margin.right;
+  const inHeight = height - margin.top - margin.bottom;
 
   var svg = d3
     .select("#canvas")
@@ -14,19 +15,31 @@ const render = (data) => {
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  var x = d3.scaleLinear().domain([0, 10000]).range([0, width]);
+  console.log(data);
+  console.log(d3.max(data, (d) => d.lifeexpectancy));
+  var x = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.purchasingpower)])
+    .range([0, width]);
   svg
     .append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
 
-  var y = d3.scaleLinear().domain([0, d3.max(data, d => d.lifeexpectancy)]).range([ height,0]);
+  var y = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.lifeexpectancy)])
+    .range([height, 0]);
   svg.append("g").call(d3.axisLeft(y));
 
-  var z = d3.scaleLinear().domain([0, d3.max(data, d=> d.population)]).range([0, 40]);
+  var z = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.population)])
+    .range([0, 60]);
 
   svg
     .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`)
     .selectAll("dot")
     .data(data)
     .enter()
@@ -48,6 +61,10 @@ const render = (data) => {
 d3.json(
   "https://gist.githubusercontent.com/josejbocanegra/000e838b77c6ec8e5d5792229c1cdbd0/raw/83cd9161e28e308ef8c5363e217bad2b6166f21a/countries.json"
 ).then((data) => {
-  this.data = data;
+  data.forEach((d) => {
+    d.population = +d.population;
+    d.lifeexpectancy = +d.lifeexpectancy;
+    d.purchasingpower = +d.purchasingpower;
+  });
   render(data);
 });
